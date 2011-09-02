d69a1783dd80efd41cc5c1f6338ffa98d93aa650
maxexcloo <max@excloo.com>
2011-09-03 00:21:54 +1000

    Added Variable Support & Optional PHP

--- server.sh
+++ server.sh
@@ -1,26 +1,68 @@
 #!/bin/bash
 cd $(dirname $0)
 
-aptitude install nginx
-mv settings/nginx /etc/
-rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-invoke-rc.d nginx restart
+function install_exim {
+	aptitude install exim4
+	sed -i "s/dc_eximconfig_configtype='local'/dc_eximconfig_configtype='internet'/" /etc/exim4/update-exim4.conf.conf
+	invoke-rc.d exim4 restart
+}
 
-aptitude install php-fpm
-mv settings/php5 /etc/
-rm /etc/php5/fpm/pool.d/www.conf
-invoke-rc.d php5-fpm restart
+function install_extra {
+	aptitude install php5-gd
+	gpasswd -a main www-data
+}
 
-aptitude install mysql-server
-invoke-rc.d mysql stop
-mv settings/mysql /etc/
-rm -f /var/lib/mysql/ib*
-invoke-rc.d mysql start
-mysql_secure_installation
+function install_mysql {
+	aptitude install mysql-server
+	aptitude install php5-mysql
+	invoke-rc.d mysql stop
+	mv settings/mysql /etc/
+	rm -f /var/lib/mysql/ib*
+	invoke-rc.d mysql start
+	mysql_secure_installation
+}
 
-aptitude install exim4
-sed -i "s/dc_eximconfig_configtype='local'/dc_eximconfig_configtype='internet'/" /etc/exim4/update-exim4.conf.conf
-invoke-rc.d exim4 restart
+function install_nginx {
+	aptitude install nginx
+	mv settings/nginx /etc/
+	rm /etc/nginx/conf.d/php.conf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+	invoke-rc.d nginx restart
+}
 
-aptitude install php5-gd php5-mysql
-gpasswd -a main www-data
+function install_php {
+	aptitude install php-fpm
+	mv settings/php5 /etc/
+	rm /etc/php5/fpm/pool.d/www.conf
+	mv settings/nginx/conf.d/php.conf /etc/nginx/conf.d/php.conf
+	invoke-rc.d php5-fpm restart
+}
+
+#################
+## Init Script ##
+#################
+
+case "$1" in
+	# Installs Basic Packages
+	basic)
+		install_nginx
+		install_php
+	;;
+	# Installs Full Packages
+	full)
+		install_nginx
+		install_php
+		install_mysql
+		install_exim
+	;;
+	# Installs Extra Packages
+	extra)
+		install_extra
+	;;
+	# Shows Help
+	*)
+		echo \>\> You must run this script with options. They are outlined below:
+		echo For a basic server install (NGINX + PHP): sh server.sh basic
+		echo For a full install (NGINX + PHP + MySQL + Exim): sh minimal.sh full
+		echo To set a user and install extra PHP libs: sh minimal.sh extra
+	;;
+esac
